88f007ec0fa886cf784136fd2d85d4332a431774
dshkol <shkolnikd@gmail.com>
2016-03-19 15:09:47 -0700

    work in progress

--- listings.R
+++ listings.R
@@ -22,8 +22,6 @@ listings <- read.csv("listings_small.csv", header = TRUE, stringsAsFactors = FAL
 
 # Take a look at the data structure
 
-str(listings)
-
 # A few of these strings are categorical factors. Let's change the type so they're easier to work with and take less memory.
 
 listings$neighbourhood <- as.factor(listings$neighbourhood)
@@ -48,6 +46,41 @@ listings %>%
   group_by(neighbourhood) %>%
   summarise(neighbourhood_median = median(price))
 
+neighbourhood_prices <- listings %>% 
+  group_by(neighbourhood, room_type) %>%
+  summarise(median_price = median(price),
+            observations = n())
 
+neighbourhood_prices[neighbourhood_prices$observations < 5,] <- NA
 
-ggplot(listings, aes)
+ggplot(na.omit(neighbourhood_prices), aes (x = median_price, y = neighbourhood, colour = room_type, size = observations)) + geom_point()
+
+# Pretty-fy the chart
+# Reorder the factor levels, so A is at the top of our charts. 
+# neighbourhood_prices$neighbourhood <- factor(neighbourhood_prices$neighbourhood, levels=rev(levels(neighbourhood_prices$neighbourhood)))
+# Fix labels and legend, add titles.
+# ggplot(na.omit(neighbourhood_prices), aes (x = median_price, y = neighbourhood, colour = room_type, size = observations)) + geom_point() + xlab("Median price per night") + ylab("Neighbourhood") + scale_size_continuous(name = "Observations", breaks = c(50,250,500,750)) + scale_colour_discrete(name="Room Type") + ggtitle("AirBnB Nightly Prices by Neighbourhood and Room Type") +theme(plot.title = element_text(face = "bold"))
+# ggsave("nightly_prices_breakdown.png")
+
+# Lets make some maps, finally. 
+
+# Using ggmap...
+
+# Make a bounding box for our data
+
+bbox <- make_bbox(longitude, latitude, listings, f = 0.1)
+vanMap <- get_map(location = bbox, zoom = 12, maptype = "toner")
+ggmap(vanMap) + geom_point(aes(x = longitude, y = latitude, colour = room_type), data = listings)
+
+# Thats messy...
+
+ggmap(vanMap) + geom_point(aes(x = longitude, y = latitude, colour = room_type), alpha = 0.75, size = 1, data = listings) + ggtitle("Map of AirBnB Listings by Room Type") + theme(legend.position = "bottom", axis.text = element_blank(), axis.title = element_blank(), axis.ticks = element_blank(), plot.title = element_text(face = "bold")) + scale_colour_discrete(name = "Room Type")
+ggsave("ggmap_example_1.png")
+
+# Still hard to look at. One of the nice things in R is how easy it is to make faceted graphics.
+
+ggmap(vanMap) + geom_point(aes(x = longitude, y = latitude, colour = room_type), alpha = 0.5, size = 0.5, data = listings) + facet_wrap(~room_type) + ggtitle("AirBnB Listings by Room Type") + theme(legend.position = "none", axis.text = element_blank(), axis.title = element_blank(), axis.ticks = element_blank(), plot.title = element_text(face = "bold") ) 
+ggsave("ggmap_example_2.png")
+
+# Lets take a look at prices for whole apartments
+
